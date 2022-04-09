b51f17aaa64d91a24d4ca50f2fe756868c6147d1
felipecabesssa <felipesoares100@hotmail.com>
2022-04-09 14:33:03 -0300

    Excecoes - Tratando erro de referencia ciclica e Categoria not found

---
+++ backend/src/main/java/br/com/bankslife/backend/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.bankslife.backend.controllers.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.bankslife.backend.services.exceptions.ObjectNotFoundException;
+import br.com.bankslife.backend.services.exceptions.StandardError;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+
+}
--- backend/src/main/java/br/com/bankslife/backend/services/CategoriaService.java
+++ backend/src/main/java/br/com/bankslife/backend/services/CategoriaService.java
@@ -2,21 +2,25 @@ package br.com.bankslife.backend.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.bankslife.backend.domains.Categoria;
 import br.com.bankslife.backend.repositories.CategoriaRepository;
+import br.com.bankslife.backend.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
-	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName()));
+	}
+
 }
