6c047c4acf08e86671f7674fb37081d684354c31
felipecabesssa <felipesoares100@hotmail.com>
2022-04-08 23:11:22 -0300

    Backend - Projeto criado

---
+++ backend/src/main/java/br/com/bankslife/backend/BankslifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.bankslife.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankslifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankslifeApplication.class, args);
+	}
+
+}
