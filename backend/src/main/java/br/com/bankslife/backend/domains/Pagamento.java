1f8379ececb36b37616da9f0297ed3f416338303
felipecabesssa <felipesoares100@hotmail.com>
2022-04-09 23:46:23 -0300

    Pedido, Pagamento (tipos com herança) e Enum estadoPagamento

---
+++ backend/src/main/java/br/com/bankslife/backend/domains/Pagamento.java
@@ -0,0 +1,83 @@
+package br.com.bankslife.backend.domains;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import br.com.bankslife.backend.domains.enums.EstadoPagamento;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Pagamento implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer id;
+	private EstadoPagamento estado;
+	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
+	private Pedido pedido;
+	
+	public Pagamento() {
+	}
+
+	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+		super();
+		this.id = id;
+		this.estado = estado;
+		this.pedido = pedido;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public EstadoPagamento getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPagamento estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
