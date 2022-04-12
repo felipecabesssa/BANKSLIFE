9ac06383e6e048279abbe446bfdc4d1f91462ce9
felipecabesssa <felipesoares100@hotmail.com>
2022-04-11 23:46:52 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ Loja/backend/src/main/java/br/com/bankslife/backend/controllers/PedidoController.java
@@ -0,0 +1,26 @@
+package br.com.bankslife.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bankslife.backend.domains.Pedido;
+import br.com.bankslife.backend.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoController {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
