a241913ea9ecc752a9a73caeea7de6cca30c974b
felipecabesssa <felipesoares100@hotmail.com>
2022-04-12 01:02:29 -0300

    CRUD - Categoria

--- Loja/backend/src/main/java/br/com/bankslife/backend/controllers/CategoriaController.java
+++ Loja/backend/src/main/java/br/com/bankslife/backend/controllers/CategoriaController.java
@@ -1,13 +1,20 @@
 package br.com.bankslife.backend.controllers;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.bankslife.backend.domains.Categoria;
+import br.com.bankslife.backend.dto.CategoriaDTO;
 import br.com.bankslife.backend.services.CategoriaService;
 
 @RestController
@@ -18,9 +25,38 @@ public class CategoriaController {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> buscaCategoriaPorId(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> buscaTodasCategorias() {
+		List<Categoria> lista = service.buscaTodasCategorias();
+		List<CategoriaDTO> listaDto = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insereCategoria(@RequestBody Categoria obj){
+		obj = service.insereCategoria(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> atualizaCategoria(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.atualizaCategoria(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> apagaCategoria(@PathVariable Integer id) {
+		service.apagaCategoria(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- Loja/backend/src/main/java/br/com/bankslife/backend/services/CategoriaService.java
+++ Loja/backend/src/main/java/br/com/bankslife/backend/services/CategoriaService.java
@@ -1,13 +1,15 @@
 package br.com.bankslife.backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.bankslife.backend.domains.Categoria;
 import br.com.bankslife.backend.repositories.CategoriaRepository;
+import br.com.bankslife.backend.services.exceptions.DataIntegrityException;
 import br.com.bankslife.backend.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +23,31 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id
 					+ ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insereCategoria(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria atualizaCategoria(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void apagaCategoria(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possua produtos");
+		}
+		
+	}
+	
+	public List<Categoria> buscaTodasCategorias(){
+		return repo.findAll();
 	}
 
 }
