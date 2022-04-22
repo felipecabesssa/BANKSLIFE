0c7789d074eda7851c0b0bb155b67e93817b5328
felipecabesssa <felipesoares100@hotmail.com>
2022-04-22 01:07:36 -0300

    Estilizando Menu

--- leilao/frontend/src/App.vue
+++ leilao/frontend/src/App.vue
@@ -16,11 +16,12 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #212529;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fcc107;
 }
 
 nav {
--- leilao/frontend/src/components/MenuLeilao.vue
+++ leilao/frontend/src/components/MenuLeilao.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="navbar-nav">
-    <ul>
+
+  <nav class="navbar navbar-expand navbar-light mb-3 bg-dark">
+    <div class="navbar-nav">
       <router-link to="/">
-        <li href="">Todos</li>
+        <a class="nav-item nav-link active text-warning" href="">Todos</a>
       </router-link>
       <router-link to="/aguardando">
-        <li href="">Aguardando</li>
+        <a class="nav-item nav-link active text-warning" href="">Aguardando</a>
       </router-link>
       <router-link to="/aprovado">
-        <li href="">Aprovado</li>
+        <a class="nav-item nav-link active text-warning" href="">Aprovado</a>
       </router-link>
       <router-link to="/entregue">
-        <li href="">Entregue</li>
+        <a class="nav-item nav-link active text-warning" href="">Entregue</a>
       </router-link>
-    </ul>
-  </div>
+    </div>
+  </nav>
+  <hr>
+
 </template>
 
 <script>
+
 export default {};
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.navbar{
+  justify-content: space-around;
+}
+
+.navbar-nav a{
+  text-decoration: none;
+     
+ }
+
 </style>
 
