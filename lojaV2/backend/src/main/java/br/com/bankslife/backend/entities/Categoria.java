2c244b07a1f87ac876a85e429ff6271ee8b705c6
felipecabesssa <felipesoares100@hotmail.com>
2022-04-22 09:43:38 -0300

    Backend LojaV2 - Entidades Categoria e Produto

---
+++ lojaV2/backend/src/main/java/br/com/bankslife/backend/entities/Categoria.java
@@ -0,0 +1,72 @@
+package br.com.bankslife.backend.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Produto> produtos = new ArrayList<>();
+	
+	public Categoria() {
+	}
+
+	public Categoria(Long id, String name) {
+		super();
+		this.id = id;
+		this.nome = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return nome;
+	}
+
+	public void setName(String name) {
+		this.nome = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ lojaV2/backend/src/main/java/br/com/bankslife/backend/entities/Produto.java
@@ -0,0 +1,101 @@
+package br.com.bankslife.backend.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Integer quantidade;
+	private Double preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoriaid")
+	private Categoria categoria;
+	
+	public Produto() {
+	}
+
+	public Produto(Long id, String nome, Integer quantidade, Double preco, Categoria categoria) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
