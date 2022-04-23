bd947375bef06baf354e225187c8139fe0d03297
felipecabesssa <felipesoares100@hotmail.com>
2022-04-23 16:00:29 -0300

    Mutations, Actions, v-on, v-mode - incremento de Produtos e cont

--- lojaV2/frontend/src/store/index.js
+++ lojaV2/frontend/src/store/index.js
@@ -2,12 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count:0,
+    produtos:[]
   },
   getters: {
   },
   mutations: {
+    increments(state){
+      state.count++
+    },
+    decrement(state){
+      state.count--
+    },
+    addProdutos(state, payload){
+      state.produtos.push(payload)
+    }
   },
   actions: {
+    addProducts({ commit }, payload){
+      commit('addProdutos', payload)
+    }
   },
   modules: {
   }
--- lojaV2/frontend/src/views/Categorias.vue
+++ lojaV2/frontend/src/views/Categorias.vue
@@ -7,16 +7,29 @@
         <div class="dados">{{ categoria.id }}</div>
         <div class="dados">{{ categoria.nome }}</div>
       </div>
+      <div class="vuex">
+        {{ count }}
+        <button class="btn btn-warning" v-on:click="increments">+</button>
+        <button class="btn btn-warning" v-on:click="decrement">-</button>
+      </div>
+      <hr>
+      <div class="produtos">
+        {{ produtos }}
+        <input v-model="products" class="form-control form-control-sm" type="text">
+        <button class="btn btn-warning" v-on:click="addProducts(products)">+</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapState, mapMutations, mapActions } from 'vuex'
 export default {
   data(){
     return{
-      categorias:{}
+      categorias:{},
+      products:''
     }
   },
   methods:{
@@ -24,10 +37,15 @@ export default {
       axios.get('http://localhost:8081/categorias').then(resp => {
         this.categorias = resp.data
       })
-    }
+    },
+    ...mapMutations(['increments', 'decrement']),
+    ...mapActions(['addProducts'])
   },
   created(){
     this.getCategorias()
+  },
+  computed:{
+    ...mapState(['count', 'produtos'])
   }
 };
 </script>
@@ -42,10 +60,32 @@ export default {
     justify-content: center;
     padding: 1%;
     display: flex;
-    border: 1px solid grey;
+    border: 1px solid #2c3e50;
     .dados {
       padding: 1%;
     }
   }
+}
+
+.vuex{
+  display: flex;
+  font-size: 30px;
+  font-weight: 900;
+  align-items: center;
+  justify-content: center;
+}
+
+.btn{
+  margin: 1%;
+  width: 50px;
+}
+
+.produtos{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+    input{
+      border-bottom: solid 1px #2c3e50;
+    }
 }
 </style>
