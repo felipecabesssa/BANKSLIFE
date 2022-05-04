dc0e5d2fc313a92d13e635594fa047d189fed0b5
felipecabesssa <felipesoares100@hotmail.com>
2022-05-03 23:18:39 -0300

    UserRepository e UserService - CRUD no Service

---
+++ lojaV2/backend/src/main/java/br/com/bankslife/backend/security/UsersRepository.java
@@ -0,0 +1,7 @@
+package br.com.bankslife.backend.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long>{
+
+}
