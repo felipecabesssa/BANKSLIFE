b4cef7fb0a9f422f38c4eb36e0d428f506fbc5f9
felipecabesssa <felipesoares100@hotmail.com>
2022-05-09 23:34:17 -0300

    Criando Formulario de login

--- lojaV2/frontend/src/views/login/Login.vue
+++ lojaV2/frontend/src/views/login/Login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="main">
-      <h2>Login</h2>
+      <div class="login">
+        <h2>Login</h2>
+        <label for="username">Usuario</label>
+        <input type="text">
+        <label for="password">Senha</label>
+        <input type="password">
+        <button class="btn btn-warning">Logar</button>
+      </div>
   </div>
 </template>
 
@@ -10,6 +17,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2%;
+    .login{
+        display: flex;
+        flex-direction: column;
+        width: 40%;
+        button{
+            margin-top: 10px;
+        }
+    }
+}
 
 </style>
