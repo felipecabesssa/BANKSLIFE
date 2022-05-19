7c2c1b1fef6a94f65f3bf2d616f24a8abbcd7ba8
felipecabesssa <felipesoares100@hotmail.com>
2022-05-18 21:31:14 -0300

    Cadastrando Usuario Frontend com email confir - pt2 - nao funfou

--- lojaV2/frontend/src/views/login/Cadastro.vue
+++ lojaV2/frontend/src/views/login/Cadastro.vue
@@ -32,7 +32,27 @@ export default {
     ...mapActions('user', ["userRegisterAction", "emailConfirmed"]),
 
     async enviarDadosCadastro(){
+
+      const user = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
+      }
       
+      await this.userRegisterAction(user);
+
+      const email = {
+        emailTo: user.email,
+        subject: "Confirmação de Cadastro",
+        text: `<h4>Confimação do Seu Cadastro Adm Fullstack</h4>
+                  <td style="border-radius: 4px; background: #0095ff; color:white; text-align: center;">
+                      <a href="http://localhost:8080/#/registration?email=${user.email}" style="background: #0095FF; border: 1px solid #0077cc; box-shadow: inset 0 1px 0 0 rgba(102,191,255,.75); font-family: arial, sans-serif; font-size: 17px; line-height: 17px; color: #ffffff; text-align: center; text-decoration: none; padding: 13px 17px; display: block; border-radius: 4px;">
+                       Confirmar Cadastro
+                   </a>
+               </td>`,
+      }
+
+      await this.emailConfirmed(email);
     }
   }
 }
