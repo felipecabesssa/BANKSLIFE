f4cbbcb37c1a6b1bf22b2d18ee8a8771e4faa629
felipecabesssa <felipesoares100@hotmail.com>
2022-06-01 23:02:15 -0300

    Upload de imagens no Frontend2

--- lojaV2/frontend/src/views/produtos/UploadImages.vue
+++ lojaV2/frontend/src/views/produtos/UploadImages.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="main">
+      <div v-if="previewImage" class="preview">
+          <img :src="previewImage" alt="Imagem">
+      </div>
       <input type="file" @change="selectImage">
-      <button class="btn btn-warning" @click="uploadImage">Upload</button>
+
+      <div
+        v-if="progress > 0"
+        class="progress-bar progress-bar-info mt-4"
+        role="progressbar"
+        :aria-valuenow="progress"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        :style="{ width: progress + '%' }"
+        >{{ progress }}%
+      </div>
+
+      <button class="btn btn-warning mt-4" @click="uploadImage">Upload</button>
   </div>
 </template>
 
@@ -11,8 +26,10 @@ export default {
 
     data(){
         return{
+            previewImage:null,
             selectedImage:null,
-            id: this.$route.params.id
+            id: this.$route.params.id,
+            progress:0
         }
     },
 
@@ -20,6 +37,7 @@ export default {
         selectImage(e){
             console.log(e.target.files[0])
             this.selectedImage = e.target.files[0]
+            this.previewImage = URL.createObjectURL(this.selectedImage)
         },
 
         uploadImage(){
@@ -27,7 +45,12 @@ export default {
             formData.append('file', this.selectedImage)
             var token = localStorage.getItem('token')
             const headers = { Authorization: `Bearer ${token}` }
-            axios.post('http://localhost:8081/fotos/' + this.id, formData, { headers }).then(resp => {
+            axios.post('http://localhost:8081/fotos/' + this.id, formData,
+             {
+                  headers,
+                  onUploadProgress: (event) => {this.progress = Math.round((100 * event.loaded) / event.total)}
+             }
+             ).then(resp => {
                 console.log(resp.data)
             })
         }
@@ -36,6 +59,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1%;
+    .preview{
+        padding: 1%;
+        max-width: 35%;
+        img{
+            max-width: 90%;
+        }
+    }
+}
 
 </style>
