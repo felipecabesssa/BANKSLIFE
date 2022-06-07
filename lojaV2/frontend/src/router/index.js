aaded58fc7277ea2ee35d20116e0f1ffc0854536
felipecabesssa <felipesoares100@hotmail.com>
2022-06-06 23:53:56 -0300

    Pagina nao encontrada - Erro 404 - Not Found

--- lojaV2/frontend/src/router/index.js
+++ lojaV2/frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   },
   {
     path: '/about',
---
+++ lojaV2/frontend/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>404</h2>
+  <h3>Página Não encontrada</h3>
+  <button class="btn btn-warning" @click="voltar">Voltar</button>
+</template>
+
+<script>
+export default {
+    methods:{
+        voltar(){
+            this.$router.go(-1)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
